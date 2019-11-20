4e9dc0d03c5f983621ee5ab782b1c69ab06b2476
Stella Cai <stella.sijiacai@gmail.com>
2019-11-20 00:36:30 -0500

    worked on random

--- datapath.v
+++ datapath.v
@@ -1,9 +1,24 @@
-module random(register_num, bit_val);
-	output [3:0] register_num;
-	output [1:0] bit_val;
-	initial
-		begin
-			register_num = $urandom%15;
-			bit_val = $urandom%2;
-		end
+// Code modified from
+// https://vlsicoding.blogspot.com/2014/07/verilog-code-for-4-bit-linear-feedback-shift-register.html
+// number occurs about 1/128 cycles
+module random (out, clock, reset);
+  input clock, reset;
+  output reg [6:0] out;
+
+
+  wire feedback;
+  reg result;
+
+  assign feedback = ~(out[6] ^ out[5]);
+
+	always @(posedge clock, negedge reset)
+	begin
+		if (~reset)
+			out = 7'b0;
+		else
+			out = {out[5:0], feedback};
+		
+		result = (out == 7'b1010111) ? 1 : 0;
+	end
 endmodule
+
